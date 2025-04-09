09774852828faf70cc59ed49f66e69b269481246
LikithaBatchu <Likithabatchu@outlook.com>
2025-04-09 21:49:15 +0530

    Adding files

---
+++ index.js
@@ -0,0 +1,14 @@
+function over(obj){
+    obj.style.backgroundColor="rgb(161, 82, 207)";
+
+}
+function out(obj) {
+    obj.style.backgroundColor="white";
+    
+}
+function on(obj){
+    obj.style.textDecoration="underline";
+}
+function of(obj){
+    obj.style.textDecoration="none";
+}
